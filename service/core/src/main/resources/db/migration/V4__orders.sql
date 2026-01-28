14bb32bd044239ab48dc3289186cb09bda6b19cc
jeon <jyh0872@gmail.com>
2026-01-28 22:25:01 +0900

    feat: order domain, entity 구현

---
+++ service/core/src/main/resources/db/migration/V4__orders.sql
@@ -0,0 +1,16 @@
+CREATE TABLE orders (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    order_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
+    stock_code VARCHAR(255),
+    order_type VARCHAR(255),
+    price DECIMAL(19, 4),
+    quantity INT,
+    status VARCHAR(255),
+    created_at DATETIME,
+    updated_at DATETIME,
+    PRIMARY KEY (id),
+    UNIQUE KEY uk_order_order_id (order_id),
+    CONSTRAINT fk_orders_user
+        FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
