ab855d2b137ee3e4f29e8463a25223f89d0260a0
jeon <jyh0872@gmail.com>
2026-01-28 22:42:23 +0900

    feat: core outbox events 테이블 구현

---
+++ service/core/src/main/java/mvest/core/global/event/infrastructure/EventType.java
@@ -0,0 +1,11 @@
+package mvest.core.global.event.infrastructure;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Getter
+@RequiredArgsConstructor
+public enum EventType {
+}
