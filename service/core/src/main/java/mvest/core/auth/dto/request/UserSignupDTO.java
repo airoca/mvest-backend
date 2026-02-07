80014be8ae129a37221c7a05917ff5772619d207
jeon <jyh0872@gmail.com>
2026-02-08 01:28:39 +0900

    refactor: UserSignupDTO 미사용 필드 삭제

--- service/core/src/main/java/mvest/core/auth/dto/request/UserSignupDTO.java
+++ service/core/src/main/java/mvest/core/auth/dto/request/UserSignupDTO.java
@@ -8,9 +8,6 @@ import java.time.LocalDate;
 public record UserSignupDTO(
         @NotNull(message = "플랫폼은 필수 값입니다.") Platform platform,
         @NotNull(message = "사용자 이름은 필수 값입니다.") String userName,
-        LocalDate birth,
-        Long regionId,
-        String introduction,
-        String authCode
+        LocalDate birth
 ) {
 }
