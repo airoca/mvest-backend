160b66a16a058cc8358f4113db3970da07bb3d89
jeon <jyh0872@gmail.com>
2026-02-07 17:22:01 +0900

    refactor: asset, core, order 서비스의 개별 outbox 삭제

--- service/asset/src/main/resources/db/migration/V4__outbox_events.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE outbox (
-    outbox_id BIGINT NOT NULL,
-    event_type VARCHAR(100) NOT NULL,
-    payload TEXT NOT NULL,
-    created_at DATETIME NOT NULL,
-    PRIMARY KEY (outbox_id)
-);
